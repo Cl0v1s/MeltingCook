770c5169526383d51aca0b5d923570b3b89fb64f
Clovis Portron <chaipokoi@gmail.com>
2017-11-15 22:57:43 +0100

    Uniformisation recherche reports

--- frontend/src/static/JS/index.js
+++ frontend/src/static/JS/index.js
@@ -15069,7 +15069,7 @@ module.exports = riot.tag2('app-adminpins', '<app-header></app-header> <app-tabb
 });
 },{"riot":8}],16:[function(require,module,exports){
 var riot = require('riot');
-module.exports = riot.tag2('app-adminreports', '<app-header></app-header> <app-tabbar tabs="{tabs}"></app-tabbar> <div class="content no-margin"> <div class="search"> <form name="search-target"> <h2>Chercher par cible</h2> <input type="text" name="target" ref="target"> <p class="hint"> Ce champ ne peut etre vide. </p> <input type="button" value="Rechercher" onclick="{showForTarget}"> <div if="{targets != null}"> <table class="dark"> <tr> <td>Nom</td><td>Action</td> </tr> <tr each="{target in targets}"> <td>{target.username}</td><td><a href="#/admin/reports/to/{target.id}">Voir</a></td> </tr> </table> </div> </form> <form name="search-author"> <h2>Chercher par auteur</h2> <input type="text" name="author" ref="author"><input type="button" value="Rechercher" onclick="{showForAuthor}"> <div if="{authors != null}"> <table class="dark"> <tr> <td>Nom</td><td>Action</td> </tr> <tr each="{author in authors}"> <td>{author.username}</td><td><a href="#/admin/reports/by/{author.id}">Voir</a></td> </tr> </table> </div> </form> </div> <app-reports reports="{reports}"></app-reports> </div> <app-footer></app-footer>', '', '', function(opts) {
+module.exports = riot.tag2('app-adminreports', '<app-header></app-header> <app-tabbar tabs="{tabs}"></app-tabbar> <div class="content no-margin"> <div class="search"> <form name="search-target"> <h2>Chercher par cible</h2> <app-userselector ref="target"></app-userselector> <input type="button" value="Rechercher" onclick="{showForTarget}"> </form> <form name="search-author"> <h2>Chercher par auteur</h2> <app-userselector ref="author"></app-userselector> <input type="button" value="Rechercher" onclick="{showForAuthor}"> </form> </div> <app-reports reports="{reports}"></app-reports> </div> <app-footer></app-footer>', '', '', function(opts) {
         var tag = this;
 
         tag.tabs = null;
@@ -15110,69 +15110,14 @@ module.exports = riot.tag2('app-adminreports', '<app-header></app-header> <app-t
 
         tag.showForTarget = function()
         {
-            var valid = new Validatinator({
-                "search-target": {
-                    "target" : "required"
-                }
-            });
-            if (valid.passes("search-target")) {
-                tag.retrieveTarget(tag.refs.target.value);
-            }
-            if(valid.fails("search-target"))
-            {
-                App.diagnosticForm("search-target", valid.errors);
-            }
-        };
-
-        tag.retrieveTarget = function(name)
-        {
-            var request = App.request(App.Address + "/getusers", {
-                "filters" : JSON.stringify({
-                    "username" : name
-                })
-            });
-            request.then(function(response) {
-                tag.targets = response.data;
-                tag.update();
-            });
-            request.catch(function(error)
-            {
-                ErrorHandler.alertIfError(error);
-            });
+            route("/admin/reports/to/"+tag.refs.target.value);
         };
 
         tag.showForAuthor = function()
         {
-            var valid = new Validatinator({
-                "search-author": {
-                    "author" : "required"
-                }
-            });
-            if (valid.passes("search-author")) {
-                tag.retrieveAuthor(tag.refs.author.value);
-            }
-            if(valid.fails("search-author"))
-            {
-                App.diagnosticForm("search-author", valid.errors);
-            }
+            route("/admin/reports/by/"+tag.refs.author.value);
         };
 
-        tag.retrieveAuthor = function(name)
-        {
-            var request = App.request(App.Address + "/getusers", {
-                "filters" : JSON.stringify({
-                    "username" : name
-                })
-            });
-            request.then(function(response) {
-                tag.authors = response.data;
-                tag.update();
-            });
-            request.catch(function(error)
-            {
-                ErrorHandler.alertIfError(error);
-            });
-        };
 });
 },{"riot":8}],17:[function(require,module,exports){
 var riot = require('riot');

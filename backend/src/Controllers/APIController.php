9bdba4b6cbef928b087bb7179ce87a6b56490055
Clovis Portron <chaipokoi@gmail.com>
2018-02-05 18:17:08 +0100

    d

--- backend/src/Controllers/APIController.php
+++ backend/src/Controllers/APIController.php
@@ -199,7 +199,7 @@ class APIController extends Controller
 
     $headr = array();
     $headr[] = 'Content-type: application/x-www-form-urlencoded';
-    $headr[] = 'Authorization:  Basic QVRxcnpvMWRYb2VJTEhWVXhFUEhDNEJ6RlFEVV82NU5QVHhyelRxa29FcU4zdFJreWthaHB4TkNO Njg0ajdtVWJ4Q3Rua3o2LUdvRnA3MHk6RUJ3a1VlamlncVJILTNUNzBGTEZBY2NWZWQxaVlJd3pM b0xtS1lPTy02YkQ0UE5ISGZJM3lyd0N0VEJTci1UYWsyaEVCdnotdXpVTmJtaGQ=';
+    $headr[] = 'Authorization:  Basic QVRxcnpvMWRYb2VJTEhWVXhFUEhDNEJ6RlFEVV82NU5QVHhyelRxa29FcU4zdFJreWthaHB4TkNONjg0ajdtVWJ4Q3Rua3o2LUdvRnA3MHk6RUJ3a1VlamlncVJILTNUNzBGTEZBY2NWZWQxaVlJd3pMb0xtS1lPTy02YkQ0UE5ISGZJM3lyd0N0VEJTci1UYWsyaEVCdnotdXpVTmJtaGQ=';
     
     curl_setopt($ch, CURLOPT_HTTPHEADER,$headr);
 
